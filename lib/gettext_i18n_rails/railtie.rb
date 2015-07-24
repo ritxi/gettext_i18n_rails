b39bc0ac699adac3b40f12beff504049ccf68aef
Ricard Forniol Agustí <ricard@forniol.cat>
2015-07-24 14:30:50 +0200

    Load gettext within railtie

--- lib/gettext_i18n_rails/railtie.rb
+++ lib/gettext_i18n_rails/railtie.rb
@@ -15,11 +15,40 @@ module GettextI18nRails
     end
 
     config.after_initialize do |app|
+      if defined?(ActiveModel) && !defined?(ActiveRecord)
+        require 'gettext_i18n_rails/active_model'
+      end
+
       if app.config.gettext_i18n_rails.use_for_active_record_attributes
         ActiveSupport.on_load :active_record do
-          require 'gettext_i18n_rails/active_model'
+          require 'gettext_i18n_rails/active_record'
         end
       end
+
+      require 'gettext_i18n_rails/gettext_hooks'
+
+      module ::GettextI18nRails
+        IGNORE_TABLES = [/^sitemap_/, /_versions$/, 'schema_migrations', 'sessions', 'delayed_jobs']
+      end
+
+      Object.send(:include, FastGettext::Translation)
+
+      # make translations html_safe if possible and wanted
+      if "".respond_to?(:html_safe?)
+        require 'gettext_i18n_rails/html_safe_translations'
+        Object.send(:include, GettextI18nRails::HtmlSafeTranslations)
+      end
+
+      # set up the backend
+      require 'gettext_i18n_rails/backend'
+      I18n.backend = GettextI18nRails::Backend.new
+
+      # make I18n play nice with FastGettext
+      require 'gettext_i18n_rails/i18n_hacks'
+
+      return unless defined?(ActionController)
+      # make bundle console work in a rails project
+      require 'gettext_i18n_rails/action_controller'
     end
   end
 end
